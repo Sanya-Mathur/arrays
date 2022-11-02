4e9bfd1ae3f12525c6a5c3f96f8f94709d65e2b2
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-02 19:29:52 +0530

    Create initializing_array.c

---
+++ initializing_array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{    printf("RA2211042010042\n");
+    int array[]={1,2,3,4,5};//initializing an array     
+    
+    for (int j=0;j<5;j++)//to print an array
+    {
+        printf("%d ",array[j]);
+        
+    }
+}
