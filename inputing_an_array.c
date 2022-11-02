a594c15d6ee1ecfd476857d705773cbba082de32
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-02 19:28:39 +0530

    Update inputing_an_array.c

--- inputing_an_array.c
+++ inputing_an_array.c
@@ -2,7 +2,6 @@
 void main()
 {    printf("RA2211042010042\n");
     int array[5];//declaring an array and defining it's size
-    int temporary_variable;
     for (int i=0;i<5;i++)//to read an array
     {
         scanf("%d",&array[i]);
