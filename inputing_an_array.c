8b46a8cfbc83c691ae734280f93b4b4c848f710d
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-02 19:20:30 +0530

    Create inputing_an_array.c

---
+++ inputing_an_array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{    printf("RA2211042010042\n");
+    int array[5];
+    int temporary_variable;
+    for (int i=0;i<5;i++)//to read an array
+    {
+        scanf("%d",&array[i]);
+        
+    }
+}
