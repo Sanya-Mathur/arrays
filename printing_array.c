810af69953c6d6bb96b587ee08469d8cb2834376
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-02 19:22:44 +0530

    Create printing_array.c

---
+++ printing_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{    printf("RA2211042010042\n");
+    int array[5];//declaring an array and defining it's size
+    int temporary_variable;
+    for (int i=0;i<5;i++)//to read an array
+    {
+        scanf("%d",&array[i]);
+        
+    }
+    for (int j=0;j<5;j++)//to print an array
+    {
+        printf("%d ",array[j]);
+        
+    }
+}
